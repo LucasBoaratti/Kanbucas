1723ae85b4fa9c765ec00392c238d353273b554d
Lucas Henrique Boaratti Silva <47123916859@>
2025-08-22 07:55:23 -0300

    Função de login adicionada, mas ainda não testada.

--- FrontEnd/src/Pages/Usuario/CadastroUsuario.jsx
+++ FrontEnd/src/Pages/Usuario/CadastroUsuario.jsx
@@ -16,6 +16,36 @@ const validacoesCadastro = z.object({
 
 export function CadastroUsuario() {
     const [cadastro, setCadastro] = useState();
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(validacoesCadastro),
+    });
+
+    async function login(data) {
+        const data = {
+            ...data,
+        }
+
+        try {
+            await axios.post("http://127.0.0.1:8000/kanbucas/usuarios", data);
+
+            setCadastro();
+        }
+        catch(error) {
+            alert("Erro ao realizar cadastro. Verifique os campos e tente novamente.");
+
+            console.log("Erro ao realizar cadastro: ", error.response?.data);
+
+            setNome("");
+            setEmail("");
+        }
+    }
     
     return (
         <main>
@@ -23,17 +53,21 @@ export function CadastroUsuario() {
                 {/* Formulário de cadastro do usuário */}
                 <section className="formularioCadastro">
                     <h1 className="titulo">Cadastro de usuários</h1>
-                    <form>
-                        <label htmlFor="nome" className="label">Nome:</label> <br />
-                        <input type="text" name="nome" id="nome" className="input" placeholder="Digite seu nome" minLength={1} maxLength={30} /> <br />
+                    {cadastro.map((data, index) => (
+                        <form key={index} onSubmit={handleSubmit(login)}>
+                            <label htmlFor="nome" className="label">Nome:</label> <br />
+                            <input type="text" name="nome" id="nome" className="input" placeholder="Digite seu nome" minLength={1} maxLength={30} value={nome} onChange={(e) => setNome(e.target.value)} {...register("nome")} /> <br />
+                            {errors.nome && <p>{errors.nome.message}</p>}
 
-                        <label htmlFor="email" className="label">Email:</label> <br />
-                        <input type="text" name="email" id="email" className="input" placeholder="Digite seu email" minLength={6} maxLength={254} /> <br />
+                            <label htmlFor="email" className="label">Email:</label> <br />
+                            <input type="text" name="email" id="email" className="input" placeholder="Digite seu email" minLength={6} maxLength={254} value={email} onChange={(e) => setEmail(e.target.value)} {...register("email")} /> <br />
+                            {errors.email && <p>{errors.email.message}</p>}
 
-                        <div className="containerBotao">
-                            <button type="submit" className="botao">Cadastrar</button>
-                        </div>
-                    </form>
+                            <div className="containerBotao">
+                                <button type="submit" className="botao">Cadastrar</button>
+                            </div>
+                        </form>
+                    ))}
                 </section>
             </section>
         </main>
