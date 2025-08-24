9f2975386285d53a9e7d974992cae6040f772fb3
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-08-24 03:21:14 -0300

    Rota para a página de cadastro de tarefas adicionada.

--- FrontEnd/src/Routes/Rotas.jsx
+++ FrontEnd/src/Routes/Rotas.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import { Index } from "../Pages/Index";
 import { CadastroUsuario } from "../Pages/Usuario/CadastroUsuario";
+import { CadastroTarefas } from "../Pages/Tarefas/CadastroTarefas";
 
 // Rotas do site
 export function Rotas() {
     return (
         <Routes>
+            {/* Rota da tela de cadastro */}
             <Route path="/">
                 <Route index element={<CadastroUsuario/>}/>
+            </Route>
+
+            {/* Rota da tela de cadastro de tarefas */}
+            <Route path="/tarefas" element={<Index/>}>
+                <Route index element={<CadastroTarefas/>}/>
             </Route>
         </Routes>
     );
