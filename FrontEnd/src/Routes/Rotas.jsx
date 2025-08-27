c08eed63deb89cafe07554bf67ff8026c7c3ac06
Lucas Henrique Boaratti Silva <47123916859@>
2025-08-27 11:36:08 -0300

    Rotas adicionadas: kanban.

--- FrontEnd/src/Routes/Rotas.jsx
+++ FrontEnd/src/Routes/Rotas.jsx
@@ -2,19 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import { Index } from "../Pages/Index";
 import { CadastroUsuario } from "../Pages/Usuario/CadastroUsuario";
 import { CadastroTarefas } from "../Pages/Tarefas/CadastroTarefas";
+import { Kanban } from "../Pages/Kanban/Kanban";
 
 // Rotas do site
 export function Rotas() {
     return (
         <Routes>
             {/* Rota da tela de cadastro */}
-            <Route path="/">
+            <Route path="/" element={<Index/>}>
                 <Route index element={<CadastroUsuario/>}/>
             </Route>
 
             {/* Rota da tela de cadastro de tarefas */}
             <Route path="/tarefas" element={<Index/>}>
                 <Route index element={<CadastroTarefas/>}/>
+            </Route>
+
+            {/* Rota da tela do kanban */}
+            <Route path="/kanban" element={<Index/>}>
+                <Route index element={<Kanban/>}/>
             </Route>
         </Routes>
     );
