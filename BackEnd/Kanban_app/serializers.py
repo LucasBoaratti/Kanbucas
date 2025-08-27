56799f26c1947561f72f7855d45d5cea917e52e4
Lucas Henrique Boaratti Silva <47123916859@>
2025-08-27 11:34:39 -0300

    Atualizações...

--- BackEnd/Kanban_app/serializers.py
+++ BackEnd/Kanban_app/serializers.py
@@ -1,16 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Usuarios, Tarefas
 
 # Classe do usuário em serializer para ser transformada em JSON 
-class UsuarioSerializer(ModelSerializer):
+class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuarios
 
         fields = "__all__"
 
 # Classe da tarefa em serializer para ser transformada em JSON
-class TarefaSerializer(ModelSerializer):
+class TarefaSerializer(serializers.ModelSerializer):
+    nome_usuario = serializers.CharField(source="id_usuario.nome", read_only=True)
+    
     class Meta:
         model = Tarefas
 
-        fields = "__all__"
+        fields = ['id', 'descricao', 'nome_setor', 'prioridade', 'id_usuario', 'nome_usuario', 'status', 'data_cadastro']
--- BackEnd/Kanban_app/urls.py
+++ BackEnd/Kanban_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UsuariosLCAPIView, TarefasLCAPIView
+from .views import UsuariosLCAPIView, TarefasLCAPIView, TarefasRUDAPIView
 
 # URLS do projeto
 urlpatterns = [
     path("usuarios", view=UsuariosLCAPIView.as_view(), name="Listar e criar usuários."),
     path("tarefas", view=TarefasLCAPIView.as_view(), name="Listar e criar tarefas."),
+    path("tarefas/<int:pk>", view=TarefasRUDAPIView.as_view(), name="Atualizar e deletar tarefas."),
 ]
